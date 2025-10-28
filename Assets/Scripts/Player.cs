cc9e16cdf5265c517396850c4595d0f45c336def
Steven Coghill <scoghill1955@gmail.com>
2025-10-27 21:10:30 -0400

    Debug HelpURL

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,26 @@ using UnityEngine;
 using System.Reflection;
 using System;
 
+/**
+ *
+ * Copyright © 2025 by Steven M. Coghill
+ * This project is licensed under the MIT License.
+ * A copy of the MIT License can be found in the 
+ * accompanying LICENSE.txt file.
+ **/
+/** 
+ * https://games.coghillclan.net/
+ * 
+ * https://www.github.com/BriarSMC/
+ *
+ * Version: 0.1.0
+ * Version History
+ * ----------------------------------------------------------------------------
+ * nn.nn.nn dd-mmm-yyyy Comment 
+ * 0.1.0    27-Oct-2025 Create based on 
+ **/
 [AddComponentMenu("Enable ActiveSceneSynchronization")]
-[HelpURL("http://localhost/mediawiki/index.php?title=Player.cs")]
+[HelpURL("https://github.com/BriarSMC/GreatMindsGame/wiki/Player.cs-HelpURL-Page")]
 public class Player : NetworkBehaviour
 {
 
@@ -13,17 +31,6 @@ public class Player : NetworkBehaviour
     {
         gameManager = FindFirstObjectByType<GameManager>();
         if (gameManager == null) throw new Exception("Could not find GameManager object.");
-    }
-
-    void Start()
-    {
-
-    }
-
-
-    void Update()
-    {
-
     }
 
     public override void OnNetworkSpawn()
