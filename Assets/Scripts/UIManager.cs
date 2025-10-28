2a9a553c35c1309e78f769356b2c45994ef3e746
Steven Coghill <scoghill1955@gmail.com>
2025-10-27 22:29:55 -0400

    Add Splash and Name Panels

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,16 +1,42 @@
+using CoghillClan.PanelManager;
 using UnityEngine;
+using System;
+
+/**
+ *
+ * Copyright © 2025 by Steven M. Coghill
+ * This project is licensed under the MIT License.
+ * A copy of the MIT License can be found in the 
+ * accompanying LICENSE.txt file.
+ **/
+/** 
+ * https://games.coghillclan.net/
+ * 
+ * https://www.github.com/BriarSMC/
+ *
+ * Version: 0.1.0
+ * Version History
+ * ----------------------------------------------------------------------------
+ * nn.nn.nn dd-mmm-yyyy Comment 
+ * 0.1.0    27-Oct-2025 From Scratch
+ **/
 
 public class UIManager : MonoBehaviour
 {
 
+    [SerializeReference] PanelManager panelManager;
+
     void Start()
     {
-        
+        panelManager = GetComponentInChildren<PanelManager>();
+
+        if (panelManager == null) throw new ApplicationException("Error in UIManger: Could not find PanelManager.");
+
     }
 
 
     void Update()
     {
-        
+
     }
 }
